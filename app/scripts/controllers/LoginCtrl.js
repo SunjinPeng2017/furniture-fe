9aa2a05e3f4875646f9100daf97034cb04133ca3
sunjinpeng <386007796@qq.com>
2017-05-20 14:09:34 +0800

    登录页面轮播图

--- app/scripts/controllers/LoginCtrl.js
+++ app/scripts/controllers/LoginCtrl.js
@@ -3,6 +3,12 @@
 angular.module('furniturefe')
     .controller('LoginCtrl', ['$rootScope', '$scope', '$location', '$cookies', '$filter', '$compile', '$timeout', function ($rootScope, $scope, $location, $cookies, $filter, $compile, $timeout) {
 
+        /**
+         * 页面初始化函数
+         */
+        let init = () => {
+            initUnsilder();
+        };
         //是否打开动画
         $scope.isLogging = false;
 
@@ -16,4 +22,22 @@ angular.module('furniturefe')
                 $scope.isLogging = false;
             }, 2000);
         };
+
+        let initUnsilder = () => {
+            $(document).ready(() => {
+                let unslider04 = $('#b04').unslider({
+                        dots: true,
+                    }),
+                    data04 = unslider04.data('unslider');
+                $('.unslider-arrow04').click(function () {
+                    var fn = this.className.split(' ')[1];
+                    data04[fn]();
+                });
+            });
+        };
+
+        /**
+         * 加载初始化函数
+         */
+        init();
     }]);
