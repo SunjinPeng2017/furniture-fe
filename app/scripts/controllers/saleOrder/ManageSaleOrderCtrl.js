4f851b5ead2748c9d539355e03dc0b473f93778a
sunjinpeng <386007796@qq.com>
2017-05-29 00:35:26 +0800

    完成销售订单的细节功能

--- app/scripts/controllers/saleOrder/ManageSaleOrderCtrl.js
+++ app/scripts/controllers/saleOrder/ManageSaleOrderCtrl.js
@@ -4,4 +4,29 @@ angular.module('furniturefe')
     .controller('ManageSaleOrderCtrl', ['$rootScope', '$scope', '$location', '$cookies', '$filter', '$compile', '$timeout', function ($rootScope, $scope, $location, $cookies, $filter, $compile, $timeout) {
 
         $rootScope.isLogged = true;
+
+        /**
+         * 要初始化函数
+         */
+        let init = () => {
+            initDateTimePicker();
+        };
+
+        /**
+         * 增加 销售订单 按钮 点击事件
+         */
+        $scope.addSaleOrder = () => {
+            $('#editRulePopupTitle').show();
+            $('#editRuleButton').show();
+            $('#rulePopupDiv').show();
+        };
+
+        /**
+         * 初始化  DateTimePicker
+         */
+        let initDateTimePicker = () => {
+            $('#datetimepicker').datetimepicker();
+        };
+
+        init();
     }]);
